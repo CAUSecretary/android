837d137a28a04b87fc33368120a5654b8d9cc1c9
yeonun <yekang.cau@gmail.com>
2022-03-30 17:15:28 +0900

    auto_phone setting

--- app/src/main/java/com/example/causecretary/ui/register/Auth_phone.kt
+++ app/src/main/java/com/example/causecretary/ui/register/Auth_phone.kt
@@ -1,12 +1,42 @@
 package com.example.causecretary.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.example.causecretary.R
+import com.example.causecretary.databinding.ActivityAuthPhoneBinding
 
-class Auth_phone : AppCompatActivity() {
+class Auth_phone : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding: ActivityAuthPhoneBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth_phone)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_auth_phone)
+
+
+        initData()
+        initView()
+    }
+
+    private fun initView() {
+        binding.clickListener = this@Auth_phone
+    }
+
+    private fun initData() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.btn_login -> {
+                Intent(this@Auth_phone,Register::class.java).run {
+                    startActivity(this)
+                }
+            }
+            R.id.ib_back -> {
+                finish()
+            }
+        }
     }
 }

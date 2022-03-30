4507ad539b7b35779d01920e0b6c0633c7d4fd4c
kimdoori <dooooreeee@naver.com>
2022-03-30 20:51:32 +0900

    register, welcome ui작업

--- app/src/main/java/com/example/causecretary/ui/register/Register.kt
+++ app/src/main/java/com/example/causecretary/ui/register/Register.kt
@@ -1,12 +1,45 @@
 package com.example.causecretary.ui.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
 import com.example.causecretary.R
+import com.example.causecretary.databinding.ActivityRegisterBinding
 
-class Register : AppCompatActivity() {
+class Register : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_register)
+
+        initData()
+        initView()
+    }
+
+    private fun initView() {
+        binding.clickListener=this@Register
+    }
+
+    private fun initData() {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.btn_auth -> {
+
+            }
+            R.id.btn_register -> {
+                Intent(this,Welcome::class.java).run {
+                    startActivity(this)
+                }
+            }
+            R.id.ib_close -> {
+
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/causecretary/ui/register/Welcome.kt
@@ -0,0 +1,11 @@
+package com.example.causecretary.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Welcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}

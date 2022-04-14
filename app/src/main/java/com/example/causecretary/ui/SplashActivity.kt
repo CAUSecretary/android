875f8108dfc9c18788078123f90a9831998ee901
dooooreeee <dooooreeee@naver.com>
2022-04-14 14:21:26 +0900

    permission,pref 완료

--- app/src/main/java/com/example/causecretary/ui/SplashActivity.kt
+++ app/src/main/java/com/example/causecretary/ui/SplashActivity.kt
@@ -1,17 +1,31 @@
 package com.example.causecretary.ui
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.causecretary.R
 import com.example.causecretary.databinding.ActivitySplashBinding
+import com.example.causecretary.databinding.PermissionDialogBinding
+import com.example.causecretary.ui.dialog.CustomDialog
+import com.example.causecretary.ui.utils.Logger
+import com.example.causecretary.ui.utils.PrefManager
+import com.example.causecretary.ui.utils.UiUtils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        const val REQUEST_CAMERA_PERMISSION = 7777
+    }
 
     lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,10 +38,28 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        CoroutineScope(Dispatchers.Default).launch {
-            delay(1000)
-            Intent(this@SplashActivity, LoginActivity::class.java).run {
-                startActivity(this)
+        //최초 앱 실행 시 permission창이 뜨고 permission체크했으면 다음부터는 그냥 넘어가도록
+        PrefManager(this@SplashActivity).apply {
+            if (isPermissionConfirm()) {
+                checkPermission()
+            } else {
+                DataBindingUtil.inflate<PermissionDialogBinding>(LayoutInflater.from(this@SplashActivity), R.layout.permission_dialog, null, false).apply {
+                    this.permissionDialog = UiUtils.showCustomDialog(
+                        this@SplashActivity,
+                        root,
+                        WindowManager.LayoutParams.MATCH_PARENT,
+                        WindowManager.LayoutParams.MATCH_PARENT,
+                        object : CustomDialog.DialogClickListener {
+                            override fun onClose() {
+                                // nothing
+                            }
+
+                            override fun onConfirm() {
+                                setPermissionConfirm(true)
+                                checkPermission()
+                            }
+                        })
+                }
             }
         }
     }
@@ -36,4 +68,50 @@ class SplashActivity : AppCompatActivity() {
        // TODO("Not yet implemented")
     }
 
+
+    /**
+     * 카메라 권한
+     **/
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(this@SplashActivity, Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+        } else {
+            goLoginpage()
+            //ViewModel 설정
+            /*getBinding()?.apply {
+                mSplashViewModel = ViewModelProvider(this@SplashActivity, SplashViewModel.Factory()).get(SplashViewModel::class.java).apply {
+                    splashViewModel = this
+                    getCurrentVersion().observe(this@SplashActivity, this@SplashActivity)
+                }
+            }
+            requestVersionOk()*/
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CAMERA_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Logger.e("doori", "granted")
+                    goLoginpage()
+                    //requestVersionOk()
+                } else {
+                    Logger.e("sigu", "not granted")
+                    goLoginpage()
+                    //requestVersionOk()
+                }
+            }
+        }
+    }
+
+    private fun goLoginpage(){
+        //프로그래스바 1초동안
+        CoroutineScope(Dispatchers.Default).launch {
+            delay(1000)
+            Intent(this@SplashActivity, LoginActivity::class.java).run {
+                startActivity(this)
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/causecretary/ui/utils/PrefManager.kt
@@ -0,0 +1,74 @@
+package com.example.causecretary.ui.utils
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+
+class PrefManager (private val context: Context) {
+
+    companion object {
+        private const val PREF_FILENAME = ""
+       // private const val PREF_LOGIN_DATA: String = "login_data"
+
+        private const val PREF_PERMISSION_FILENAME = "com.example.causecretary.premission"
+        private const val PREF_PERMISSION_CONFIRM = "permission_confirm"
+    }
+
+    fun clearAll() {
+        val prefs: SharedPreferences = context.getSharedPreferences(PREF_FILENAME, Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.clear()
+        editor.apply()
+    }
+
+    /**
+     * 로그인 토큰 저장
+     *
+     * @param loginId
+     * @param token
+     * @param refreshToken
+     */
+ /*   fun setLoginData(loginId: String, token: String, refreshToken: String) {
+        val prefs: SharedPreferences = context.getSharedPreferences(PREF_FILENAME, Context.MODE_PRIVATE)
+
+        Gson().apply {
+            val loginData: LoginData = LoginData()
+            loginData.loginId = loginId
+            loginData.token = token
+            loginData.refreshToken = refreshToken
+
+            val jsonToString: String = toJson(loginData)
+            prefs.edit().putString(PREF_LOGIN_DATA, jsonToString).apply()
+        }
+    }
+*/
+    /**
+     * 로그인 토큰 반환
+     *
+     * @return LoginData?
+     */
+  /*  fun getLoginData(): LoginData? {
+        val prefs: SharedPreferences = context.getSharedPreferences(PREF_FILENAME, Context.MODE_PRIVATE)
+        Gson().apply {
+            val jsonToString: String = prefs.getString(PREF_LOGIN_DATA, "")?:""
+            return fromJson(jsonToString, LoginData::class.java)
+        }
+    }
+
+    *//**
+     * 로그인 토큰 삭제
+     *//*
+    fun removeLoginData() {
+        val prefs: SharedPreferences = context.getSharedPreferences(PREF_FILENAME, Context.MODE_PRIVATE)
+        prefs.edit().remove(PREF_LOGIN_DATA).apply()
+    }
+*/
+    fun setPermissionConfirm(isConfirm: Boolean) {
+        val prefs: SharedPreferences = context.getSharedPreferences(PREF_PERMISSION_FILENAME, Context.MODE_PRIVATE)
+        prefs.edit().putBoolean(PREF_PERMISSION_CONFIRM, isConfirm).apply()
+    }
+
+    fun isPermissionConfirm(): Boolean {
+        val prefs: SharedPreferences = context.getSharedPreferences(PREF_PERMISSION_FILENAME, Context.MODE_PRIVATE)
+        return prefs.getBoolean(PREF_PERMISSION_CONFIRM, false)
+    }
+}

dc0a2aba1816e3c35f9cfc470161923f7ddc8a1a
jobum97 <brian654321@naver.com>
2022-05-28 21:43:13 +0900

    navi feature

--- app/src/main/java/com/example/causecretary/ui/api/ApiService.kt
+++ app/src/main/java/com/example/causecretary/ui/api/ApiService.kt
@@ -8,6 +8,7 @@ import retrofit2.http.*
 class ApiService {
     companion object{
         val DOMAIN = "http://192.168.138.85:9000/"
+        val NAVIDOMAIN = "http://192.168.219.106:8080/"
     }
 }
 
@@ -21,5 +22,13 @@ interface RetrofitApi{
     @FormUrlEncoded
     @POST("auth/login")
     fun login(@Field ("email")email:String, @Field("password") password: String): Call<RegisterResponse>
+
+    @FormUrlEncoded
+    @POST("/search/")
+    fun searchRoute(
+        @Field("endPoint") endPoint:String,
+        @Field("curLat") curLat:String,
+        @Field("curLon") curLon:String
+    ) : Call<String>
 
 }
